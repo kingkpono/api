586279ef8aa33548c0d068a3598f19e5e6a3247b
Kpono Akpabio <kingkpono@gmail.com>
2019-12-13 11:37:52 +0100

    Spring project scaffolded and LDAP authentication configured

---
+++ src/main/java/com/api/api/HomeResource.java
@@ -0,0 +1,16 @@
+package com.api.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+	
+	@GetMapping("/")
+	public String index()
+	{
+		return "Home page";
+		
+	}
+
+}
